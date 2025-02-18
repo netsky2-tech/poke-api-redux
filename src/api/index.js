2a124e43bac0c8fd5f4f1d59433fae6820c7bc38
omorales <octaviomr9810@gmail.com>
2025-02-18 15:27:56 -0600

    add api service

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const getPokemon = () => {
+    return axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then((res) => res.data.results)
+    .catch((err => console.error(err)))
+}
+
+export { getPokemon }
